1698fc6933bd6890b841491b28176694e76e7071
Sreejith K S <Sreejithsathyans@gmail.com>
2026-08-14 00:02:28 +0530

    day-09-async-await-initial-commit

---
+++ 09-dotnet-async-await/async-await-internals/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Task<int> StudentCountTask = CountTheStudents();
+Console.WriteLine(StudentCountTask.GetType());
+
+int StudentsCountFromTask = await StudentCountTask;
+Console.WriteLine(StudentsCountFromTask);
+
+int StudentsCount = await CountTheStudents();
+Console.WriteLine(StudentsCount);
+
+async Task<int> CountTheStudents()
+{
+    Console.WriteLine("** counting the students");
+    await Task.Delay(2000);
+    return 12;
+}
